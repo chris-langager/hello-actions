83d101d393cbc812f5e7de6cf3a0a933a01cc434
Chris Langager <chris.langager@shopify.com>
2021-12-08 18:38:57 -0700

    Refactor

--- index.js
+++ index.js
@@ -9,44 +9,35 @@ const github = require('@actions/github');
 const baseDir = process.cwd()
 const git = simpleGit({ baseDir })
 
-const directory = 'text_files'
+const inputDirectory = 'text_files'
+const outputDirectory = 'generated'
 const outputFile = 'together.txt'
 
 async function run(){
     try {
-       
-        const files = await fs.readdir(directory);
+
+        //Read in the files we want to stitch together
+        const files = await fs.readdir(inputDirectory);
 
         let acc = '';
         for (let file of files) {
-            const data = await fs.readFile(path.join(directory, file), 'utf8');
+            const data = await fs.readFile(path.join(inputDirectory, file), 'utf8');
             acc += data.endsWith('\n') ? data : data + '\n';
         }
 
-        console.log(acc);
-
-        const outputFilePath = path.join(directory, outputFile);
+        //combine the files and write them to a new one
+        const outputFilePath = path.join(outputDirectory, outputFile);
         await fs.writeFile(outputFilePath, acc)
 
+
+        //push up our new file
+        const { pusher }  = github.context.payload
         await git
-            .addConfig('user.email', 'christopher.langager@gmail.com')
-            .addConfig('user.name', 'Chris Langager')
+            .addConfig('user.email', pusher.email)
+            .addConfig('user.name', pusher.name)
             .add(outputFilePath)
             .commit(`updated ${outputFilePath}`)
             .push('origin', 'main');
-
-
-        // `who-to-greet` input defined in action metadata file
-        const nameToGreet = core.getInput('who-to-greet');
-        console.log(`Hello ${nameToGreet}!`);
-        
-        const time = (new Date()).toTimeString();
-        core.setOutput("time", time);
-      
-        // Get the JSON webhook payload for the event that triggered the workflow
-        const payload = JSON.stringify(github.context.payload, undefined, 2)
-        console.log(`The event payload: ${payload}`);
-      
       } catch (error) {
         core.setFailed(error.message);
       }
